68328ac745addbfce674bb07c6fe93b6abb1366d
Sourav Prasad <souravpd007@gmail.com>
2019-01-26 18:25:50 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+require_once("views/header.template.php");
+require_once("views/home.php");
+require_once("views/footer.template.php");
+?>
---
+++ views/home.php
@@ -0,0 +1,160 @@
+<?php function albumCard(){?>
+          <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#55595c" width="100%" height="100%"/><text fill="#eceeef" dy=".3em" x="50%" y="50%">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div> <?php 
+      } ?>
+  <div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <?php mainNav(); ?>
+        
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>User Section</span>
+          <a class="d-flex align-items-center text-muted" href="#">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Merchandise
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Calendar
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark my-4 w-100">
+            <div class="col-md-6 px-0">
+              <h1 class="display-4 font-italic">IIT ISM Gymkhana</h1>
+              <p class="lead my-3">You have arrived at the online residence of IIT Dhanbad's Gymkhana. At IIT Dhanbad we undertake a variety of cultural activities, which are broadly classified under 11 genres – Dance, Dramatics, Film and Media(F&M), Literary Arts, Music, Photography and Fine Arts(PFA), Speaking, Indian Languages, Lifestyle, Design and Classical & Folk Arts.<p class="lead mb-0"><a href="#" class="text-white font-weight-bold">TO LIFE. TO SURPRISES. TO DISCOVERY</a></p>
+            </div>
+      </div>
+    <div class="row mb-0">
+    <div class="col-md-6">
+      <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+        <div class="card-body d-flex flex-column align-items-start">
+          <strong class="d-inline-block mb-2 text-primary">Cultural</strong>
+          <h3 class="mb-0">
+            <a class="text-dark" href="#">Featured Event</a>
+          </h3>
+          <div class="mb-1 text-muted">Jan 23</div>
+          <p class="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, vel eius fugiat hic dolorem facilis?</p>
+          <a href="#">Continue reading</a>
+        </div>
+        <svg class="bd-placeholder-img card-img-right flex-auto d-none d-lg-block" width="250" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#55595c" width="100%" height="100%"/><text fill="#eceeef" dy=".3em" x="50%" y="50%">Thumbnail</text></svg>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+        <div class="card-body d-flex flex-column align-items-start">
+          <strong class="d-inline-block mb-2 text-success">Council</strong>
+          <h3 class="mb-0">
+            <a class="text-dark" href="#">Featured News</a>
+          </h3>
+          <div class="mb-1 text-muted">Jan 26</div>
+          <p class="card-text mb-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod cum explicabo velit error aliquid eveniet!</p>
+          <a href="#">Continue reading</a>
+        </div>
+        
+        <svg class="bd-placeholder-img card-img-right flex-auto d-none d-lg-block" width="250" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect fill="#55595c" width="100%" height="100%"/><text fill="#eceeef" dy=".3em" x="50%" y="50%">Thumbnail</text></svg>
+        </div>
+    </div>
+  </div>
+<div class="container">
+<link href="https://fonts.googleapis.com/css?family=Lora:400,400i|Playfair+Display:400,700" rel="stylesheet">
+<div class="grid">
+  <div class="flex justify-center pv4 items-center item item-b flex justify-center pv4 items-center">
+    <div class="w-80 tc">
+      <p class="b ttu f6 sans-serif">Idee</p>
+      <h1 class="playfair b lh-title mv0 pv3">I tormenti del giovane scittore di sinstra</h1>
+      <p class="lora"><span class="i">Filippo Bologna</span> – p. 150</p>
+    </div>
+  </div>
+  <div class="flex justify-center pv5 items-center item item-c">
+    <div class="w-80 tc pv4">
+      <p class="b ttu f6 sans-serif">Fiction</p>
+      <h1 class="playfair b lh-title f1 mv0 pt3">John Cheever<br>non fa riflettere</h1>
+      <p class="pb4 pt3 lora"><span class="i">Alessandro Piperno</span> – p. 156</p>
+      <p class="lora w-90 center lh-title f5">«La narrativa deve illuminaire, esplodere, ristorare. Non c'è alcuna filosofia morale nella narrativa oltre all'eccellenza. L'intensità della sensibilità e la rapidità le ho sempre ritenute importantissime. La gente cerca sempra la morale perché
+        confonde narrative e filosofia»
+    </div>
+  </div>
+  <div class="flex justify-center pv4 items-center item item-d">
+    <div class="w-80 tc">
+      <p class="b ttu f6 sans-serif">Idee</p>
+      <h1 class="playfair b lh-title f3 mv0 pv3">Mondadori e Rizzoli nel 2050</h1>
+      <p class="lora"><span class="i">Giulia Iannuzzi</span> – p. 151</p>
+    </div>
+
+  </div>
+  <div class="flex justify-center pv4 items-center item item-e">
+    <div class="w-80 tc">
+      <p class="b ttu f6 sans-serif">Idee</p>
+      <h1 class="playfair b lh-title f3 mv0 pv3">Contro<br>Tondelli</h1>
+      <p class="lora"><span class="i">Andrea Gentile</span> – p. 150</p>
+    </div>
+  </div>
+  <div class="flex justify-center pv4 items-center item item-f">
+    <div class="w-90 tc">
+      <p class="b ttu f6 sans-serif">Idee</p>
+      <h1 class="playfair b lh-title f3 mv0 pv3">Dopo 70 anni <br>non è più tempo<br> di cattocommunisti</h1>
+      <p class="lora"><span class="i">Guido Vitiello</span> – p. 154</p>
+    </div>
+  </div>
+  <div class="flex justify-center pv4 items-center item item-g">
+    <div class="w-80 tc">
+      <p class="b ttu f6 sans-serif">libri</p>
+      <p class="lora f8 lh-title pt3 pb2">Paul Vacca, <span class="i">Fulmin Tom</span>. Le avventure di un ragazzino geniale, supereroe del Sessantotto</p>
+      <p class="lora pb1"><span class="i">Nadia Terranova</span> – p. 158</p>
+      <p class="lora f8 lh-title pt2 pb1">Alberto Manguel, <span class="i">Storia naturale della curlosità</span>. Un saggio che è una camera delle meraviglie</p>
+      <p class="lora"><span class="i">Antonio Sgobba</span> – p. 158</p>
+      <p class="lora f8 lh-title pt4 pb1">Andrea Tarabbia, <span class="i">Il giardino delle mosche</span>. Romanzo visionario sulla confessione del mostro di Rostov</p>
+      <p class="lora"><span class="i">Marco Rossari</span> – p. 159</p>
+    </div>
+  </div>
+  <div class="flex justify-center pv4 items-center item item-h">
+
+    <div class="w-80 tc">
+      <p class="b ttu f6 sans-serif">non fiction</p>
+      <h1 class="playfair b lh-title mv0 pv3">Com&#8217;è difficile<br>riconoscere gli errori</h1>
+      <p class="lora"><span class="i">Giorgio Fontana</span> – p. 160</p>
+    </div>
+  </div>
+  <div class="flex justify-center pv4 items-center item item-i">
+    <div class="w-80 tc">
+      <h1 class="f4 ttu tracked playfair b mv0 pb3">Lettera da New York</h1>
+      <p class="lora"><span class="i">Paul Berman</span> – p. 162</p>
+    </div>
+  </div>
+  
+</div>
+</div>
+   

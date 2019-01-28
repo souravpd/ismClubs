faaae3704fb9ce199c49a8cf0fa78ba3985af424
Sourav Prasad <souravpd007@gmail.com>
2019-01-29 00:15:36 +0530

    Fixed Bug

---
+++ views/components.php
@@ -0,0 +1,40 @@
+<?php function mainNav(){ ?>
+
+<ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link " href="#">
+          
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">            
+            Clubs
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+          <span class="sr-only">(current)</span>
+            Events
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Blog
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Facilities
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="layers"></span>
+            Council
+          </a>
+        </li>
+      </ul>
+<?php } ?>
+
+

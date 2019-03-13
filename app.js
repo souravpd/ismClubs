16f270e545de7f6152f9d3d1d6999cee1dd1e05c
cjchirag7 <cjchirag7@gmail.com>
2019-03-13 18:40:59 +0530

    Fixed bug in app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const searchInput = document.querySelector(".form-control");
 var searchList = document.createElement("ul");
-searchList.className="list-group";
-document.querySelector("main").insertBefore(searchList,document.querySelector("main").firstChild);
+searchList.className = "list-group";
+document.querySelector("main").insertBefore(searchList, document.querySelector("main").firstChild);
 const searchResults = document.querySelector(".list-group");
 searchInput.addEventListener("keyup", search);
-function add(obj){
+function add(obj) {
     const li = document.createElement('li');
     li.className = "list-group-item";
-    searchResults.style.position="absolute";
+    searchResults.style.position = "fixed";
     searchResults.style.zIndex = "10";
-    searchResults.style.width="92.5%";
+    searchResults.style.width = "92.5%";
     const link = document.createElement('a');
     link.innerHTML = `Name : ${obj.field} <br> Type : ${obj.type} <br>Website : <a href="${obj.website}">Go there </a> <br>`;
     li.appendChild(link);
@@ -29,11 +29,11 @@ function search(e) {
     if(searchValue !== ""){
         data.forEach(function(item){
             if(item.field.toLowerCase().indexOf(searchValue)!== -1){
-                  
+                add(item);                       
             }
             else if(item.type.toLowerCase().indexOf(searchValue)!== -1){
-            }
-        })
+                add(item);                       
+}        })
     }
     e.preventDefault();
 }
---
+++ search_urls.js
@@ -0,0 +1,95 @@
+const data = [
+    {
+        "field":"LCI (Lights Camera Ism)",
+        "type":"Videography/Filmography Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/lci.html"
+    },
+    {
+        "field":"ADC (Abhay Dramatics Club)",
+        "type":"Dramatics Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/adc.html"
+    },
+    {
+        "field":"Art freaks",
+        "type":"Painting Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/art.html"
+    },
+    {
+        "field":"FotoFreaks",
+        "type":"Photography Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/foto.html"
+    },
+    {
+        "field":"LITM (Legends In The Making)",
+        "type":"Dance Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/litm.html"
+    },
+    {
+        "field":"WTC (We The Crew)",
+        "type":"Dance Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/wtc.html"
+    },
+    {
+        "field":"Manthan",
+        "type":"Music Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/manthan.html"
+    },
+  {
+        "field":"LITC (Literary Club)",
+        "type":"literature club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/litc.html"
+    },
+  {
+        "field":"Mic.Drop ToastMasters Club",
+        "type":"Literature Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/toast.html"
+    },
+  {
+        "field":"Chayanika Sangh",
+        "type":"Literature Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/chayan.html"
+    },
+  
+  {
+        "field":"Cyberlabs (Official Programming Club)",
+        "type":"Programming Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/cyber.html"
+    },
+  
+  {
+        "field":"RoboISM (Robotics and AI Club)",
+        "type":"Technical Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/robo.html"
+    },
+  
+  {
+        "field":"Mechismu",
+        "type":"Formula Racing Club",
+        "website":"https://iit-ism-clubs.000webhostapp.com/clubs/mechismu.html"
+    },
+   
+  {
+      "field":"Book Club",
+      "type":"Literary Club",
+      "website":"https://iit-ism-clubs.000webhostapp.com/clubs/book.html"
+  },
+  
+  {
+      "field":"E-Cell",
+      "type":"Entrepreneurship Club",
+      "website":"https://iit-ism-clubs.000webhostapp.com/clubs/e_cell.html"
+  },
+  
+  {
+      "field":"Arka",
+      "type":"Astronomy Club",
+      "website":"https://iit-ism-clubs.000webhostapp.com/clubs/astro.html"
+  },
+  
+  {
+      "field":"Srijan",
+      "type":"Cultural Fest",
+      "website":"https://www.srijaniitism.org/"
+  }
+  ];
+  

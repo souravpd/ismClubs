ebf86a39af96ea778aea844aed40d4562be3ea14
cjchirag7 <cjchirag7@gmail.com>
2019-03-13 18:17:05 +0530

    Trial on search result display of app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var cnt=0;
 const searchInput = document.querySelector(".form-control");
 var searchList = document.createElement("ul");
 searchList.className="list-group";
@@ -8,10 +7,7 @@ searchInput.addEventListener("keyup", search);
 function add(obj){
     const li = document.createElement('li');
     li.className = "list-group-item";
-    searchResults.style.position="absolute";
-    if(cnt==1) {
-        searchResults.style.padding-top=searchInput.offsetTop; 
-    }
+    searchResults.style.position="fixed";
     searchResults.style.zIndex = "10";
     searchResults.style.width="92.5%";
     const link = document.createElement('a');
@@ -33,12 +29,9 @@ function search(e) {
     if(searchValue !== ""){
         data.forEach(function(item){
             if(item.field.toLowerCase().indexOf(searchValue)!== -1){
-                add(item);    
-                if(!cnt) cnt++;                  
+                  
             }
             else if(item.type.toLowerCase().indexOf(searchValue)!== -1){
-                add(item);     
-                if(!cnt) cnt++;                  
             }
         })
     }
